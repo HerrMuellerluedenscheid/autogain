9a73c6a5a2b8362147bd33f14ec69411b260470d
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-03-11 17:57:31 +0100

    added event labels

--- util_optic.py
+++ util_optic.py
@@ -6,13 +6,14 @@ from collections import OrderedDict
 class Optics():
     def __init__(self, autogain):
         self.autogain = autogain
-        
+
     def plot(self):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         event_count = 0
-        for section in sorted(self.autogain.get_results(), key=lambda x:
-                              x.event.magnitude):
+        sections = sorted(self.autogain.get_results(), key=lambda x:
+                              x.event.magnitude)
+        for isection, section in enumerate(sections):
             event_count += 1
             for nslc_id in self.autogain.all_nslc_ids:
                 try:
@@ -25,12 +26,16 @@ class Optics():
                         c=self.color(nslc_id), 
                         ms=2+1.5**section.event.magnitude,
                         label=nslc_id)
-
         handles, labels = ax.get_legend_handles_labels()
         by_label = OrderedDict(zip(labels, handles))
         ax.legend(by_label.values(), by_label.keys())
-
+        self.add_event_labels(ax, sections)
         self.add_mean_lines(ax)
+
+    def add_event_labels(self, ax, sections):
+        ymin, ymax = ax.get_ylim()
+        for i_s, s in enumerate(sections):
+            ax.text(i_s, ymax, s.event.name, rotation='vertical')
 
     def add_mean_lines(self, ax):
         ids, _means = self.autogain.mean
@@ -42,7 +47,7 @@ class Optics():
     def set_color(self):
         want = self.autogain.all_nslc_ids
         self._color_dict = dict(zip(want, num.linspace(0, 1, len(want))))
-    
+
     def color(self, nslc_id):
         try:
             return cm.gist_rainbow(self._color_dict[nslc_id])
