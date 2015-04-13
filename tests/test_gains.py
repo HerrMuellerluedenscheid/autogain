a915423f90d502db5f1c14ba5ff7ec04e885ee95
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-04-13 16:09:28 +0200

    added test

---
+++ tests/test_gains.py
@@ -0,0 +1,14 @@
+import unittest
+from autogain import gains
+
+class TestGains(unittest.TestCase):
+
+    def test_dumpnload(self):
+        g = gains.Gains()
+        key = ('a','b','c','d')       
+        val = 1.1
+        g.trace_gains = {key: val}
+        print g.load(g.dump())
+
+if __name__=='__main__':
+    unittest.main()
